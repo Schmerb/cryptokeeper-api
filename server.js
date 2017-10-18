6560c634e38974799a03d16bdbc81047cfe9b4b0
Schmerb <mike.schmerbeck@gmail.com>
2017-10-17 22:22:33 -0400

    added cors in res header

--- server.js
+++ server.js
@@ -39,15 +39,15 @@ app.use(
         origin: CLIENT_ORIGIN
     })
 );
-// app.use(function(req, res, next) {
-//       res.header('Access-Control-Allow-Origin', '*');
-//       res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-//       res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-//       if (req.method === 'OPTIONS') {
-//           return res.sendStatus(204);
-//       }
-//       next();
-// });
+app.use(function(req, res, next) {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+      if (req.method === 'OPTIONS') {
+          return res.sendStatus(204);
+      }
+      next();
+});
 
 // MIDDLEWARE
 app.use(passport.initialize());
