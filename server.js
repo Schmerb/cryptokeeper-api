11f8b73d1fac0ca5906c3785cfe8c6e239076e8a
Schmerb <mike.schmerbeck@gmail.com>
2017-10-17 21:50:12 -0400

    removed client cors config

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ require('./services/crypto-prices')(httpServer);
 app.use(morgan('common'));
 
 // CORS
-app.use(
-    cors({
-        origin: CLIENT_ORIGIN
-    })
-);
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
 // app.use(function(req, res, next) {
 //       res.header('Access-Control-Allow-Origin', '*');
 //       res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
