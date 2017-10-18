e652bc453d80298008427d13af5be3d52cadd257
Schmerb <mike.schmerbeck@gmail.com>
2017-10-17 22:35:14 -0400

    removed endslash from client origian for CORS

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ exports.TEST_DATABASE_URL = (
     process.env.TEST_DATABASE_URL ||
     'mongodb://localhost/test-react-db'
 );
-exports.CLIENT_ORIGIN = 'https://cryptokeeper.netlify.com/';
+exports.CLIENT_ORIGIN = 'https://cryptokeeper.netlify.com';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'; 
