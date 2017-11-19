5dc87082962db3b112152ff1e58ac4bf862b803d
Schmerb <mike.schmerbeck@gmail.com>
2017-11-18 19:05:12 -0500

    changed client origin to custom domain

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ exports.TEST_DATABASE_URL =
     global.TEST_DATABASE_URL ||
     'mongodb://localhost/react-test-db';
 
-exports.CLIENT_ORIGIN = 'https://cryptokeeper.netlify.com';
+exports.CLIENT_ORIGIN = 'https://www.cryptokeeper.co';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'; 
